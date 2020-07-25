ec2cb71f4432ae472de416c1478c499ce988f10f
skira <lucaschiavini@hotmail.com>
2020-07-25 07:43:01 -0300

    dev cleaned

--- Bluetooth/ble_comm/README.md
+++
@@ -1,3 +0,0 @@
-# Python - Esp32 communication example :
-
-Dont forget to import the environment.yml file in the root of the repository and activate the environment before running the code.
--- Bluetooth/ble_comm/main/SensorDataType.h
+++
@@ -1,6 +0,0 @@
-#include "mpu/types.hpp"
-#define SENSORS_QUANTITY 5
-typedef struct sensor_data{
-    mpud::raw_axes_t accelRaw;     // holds x, y, z axes as int16
-    mpud::raw_axes_t gyroRaw;  
-} SensorData;
